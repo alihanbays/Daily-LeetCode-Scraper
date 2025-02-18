c0baac4dbc80a4e879809fbd6ae063068b0ab10a
alihanbays <baysala@oregonstate.edu>
2025-02-17 21:51:00 -0700

    feat: Managed to move from leetcode.com to daily question website.

--- DailyLeetCodeScraper/LeetCodeScraper.cs
+++ DailyLeetCodeScraper/LeetCodeScraper.cs
@@ -1,20 +1,32 @@
 ﻿using DailyLeetCodeScraper.Modal;
 using HtmlAgilityPack;
 using HtmlAgilityPack.CssSelectors.NetCore;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace DailyLeetCodeScraper;
 
 public class LeetCodeScraper 
 {
-    private const string Url = "https://leetcodepotd.vercel.app/";
+    private const string Url = "https://leetcode.com/problemset/";
 
     public Question GetDailyQuesiton()
     {
-        HtmlDocument? htmlDocument = new HtmlWeb().Load(Url);
-        IList<HtmlNode>? title = htmlDocument.QuerySelectorAll("div");
-        
-        Console.WriteLine(title);
-        
-        return new Question();    
+        using (IWebDriver driver = new ChromeDriver()) 
+        {
+            driver.Navigate().GoToUrl(Url);
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            wait.Until(d => d.FindElement(By.Id("__next")));
+            
+            IWebElement body = driver.FindElement(By.Id("__next"));
+            IWebElement dailyQuestion = body.FindElement(By.CssSelector("[role='rowgroup'] div"));
+            IWebElement icon = dailyQuestion.FindElement(By.CssSelector("div.mx-2.flex.items-center.py-\\[11px\\] a"));
+            icon.Click();
+            WebDriverWait wait2 = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            
+            Console.WriteLine(driver.Url);
+        }
+        return new Question();
     }
 }
